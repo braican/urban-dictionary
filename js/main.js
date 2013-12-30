f3099f6a643951c3f14649e2d94f6a328cf167d6
Nick Braica <nbraica@tankdesign.com>
2013-12-30 16:13:46 -0500

    some sweet fonts and a loading animation

--- js/main.js
+++ js/main.js
@@ -4,16 +4,16 @@
 
 (function(URBAN, $, undefined){
 
-    var DEFS = {};
+    URBAN.DEFS = {};
 
     function renderTheGame(){
-        $.each(DEFS, function(index, val) {
+        $.each(URBAN.DEFS, function(index, val) {
             var $words = $('#words li'),
                 $defs = $('#defs li');
             if($words.length === 0){
                 $('#words').append('<li>' + index + '</li>');
             } else {
-                $words.eq(Math.floor(Math.random()*$words.length)).before('<li>' + index + '</li>');    
+                $words.eq(Math.floor(Math.random()*$words.length)).before('<li>' + index + '</li>');
             }
             
             if($defs.length === 0){
@@ -31,26 +31,31 @@
             $.get('http://api.urbandictionary.com/v0/define?term=' + link, function(data) {
                 var word = data.list[0].word,
                     def = data.list[0].definition;
-                console.log(data);                
-                DEFS[word] = def;
+                URBAN.DEFS[word] = def.replace(/[\n\r]/g, ' ');
 
                 if(x > 1){
                     getDefs(x - 1);
                 } else {
                     renderTheGame();
+
+                    $('#loading').fadeOut(function() {
+                        $('#the-game').fadeIn();    
+                    });
+                    
                 }
             });
         });
     }
 
     URBAN.init = function(){
-        getDefs(4);
+        getDefs(12);
 
         $(document).on('click', 'ul li', function(event){
             event.preventDefault();
             
             $(this).parent().find('li.selected').removeClass('selected');
             $(this).addClass('selected');
+            $('#result > span').fadeOut();
 
             var $word = $('#words li.selected'),
                 $defs = $('#defs li.selected');
@@ -59,7 +64,7 @@
                 var word = $('#words li.selected').text(),
                     def = $('#defs li.selected').text();
 
-                if(DEFS[word] == def){
+                if(URBAN.DEFS[word] == def){
                     console.log("got it");
                     $('#result .gotit').fadeIn();
                     $('li.selected').removeClass('selected').addClass('correct');
